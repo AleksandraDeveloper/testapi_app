b94106cc367cbaa228cbd17794e57380dfaddddd
AleksandraDeveloper <alexandra.konst15@gmail.com>
2024-09-30 08:35:44 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,17 +1,13 @@
-# этот файл описывает подключение к БД
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL") #"postgresql+psycopg2://postgres_user:postgres_password@db:5432/superapp_db" #?client_encoding=utf8
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-# сешионмэйкер нужен для создания сессий
 session_local = sessionmaker(
     autoflush=False, autocommit=False, bind=engine
-)  # аргументы отключают автосинхронизацию с БД и автокомит
+)
 
 Base = declarative_base()
-# эта функция на основе моделей создает таблицы в БД
